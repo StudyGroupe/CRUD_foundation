02916b2835b6abe3a6d5d06bcbf622328876555b
中村広樹 <hiroki@mbp.local>
2018-11-14 23:16:49 +0900

    validate.jsの作成とcreate.jsへの適応

---
+++ js/validate.js
@@ -0,0 +1,15 @@
+$(function(){
+    alert('hoge');
+    $("#check").on("click", function(){
+        if(document.getElementById("user_name").value == "" ||
+            document.getElementById("user_pass").value == ""){
+            alert("There is an empty item\nPlease enter.");
+            return false;
+        }
+        if(document.getElementById("user_pass").value !=
+            document.getElementById("user_pass2").value){
+            alert('Password doesn'+'t match.');
+            return false;
+        }
+    });
+})
