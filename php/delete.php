22b819ad438f5349039bc18483e3e54677cafb55
中村広樹 <hiroki@mbp.local>
2018-11-23 17:55:41 +0900

    deleteファイルを修正

---
+++ php/delete.php
@@ -0,0 +1,56 @@
+<?php
+$id = $_GET['id'];
+//DB接続情報
+$DATABASE_SOURCENAME = 'mysql:dbname=crud_foundation;hostname=localhost;';
+$DATABASE_USERNAME = 'root';
+$DATABASE_PASSWORD = 'root';
+
+try{
+    $dbh = new PDO($DATABASE_SOURCENAME,$DATABASE_USERNAME,$DATABASE_PASSWORD);
+    $sql = "select name,tel from user where id=$id";
+    $stmt = $dbh->query($sql);
+    foreach($stmt as $row){}
+    $dbh = null;
+}catch(PDOException $e){
+    $e->getMessage();
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>DELETE</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>DELETE USER</h1>
+    </header>
+    <form action="delete_done.php" method="post">
+        <table>
+            <tr>
+                <th>NAME:</th>
+                <td><?php echo $row['name'] ?></td>
+            </tr>
+            <tr>
+                <th>TEL:</th>
+                <td><?php echo $row['tel'] ?></td>
+            </tr>
+        </table>
+        <div class="form-item">
+            <input type="submit" id="check" value="CHECK">
+        </div>
+
+        <div class="form-item">
+            <input type="button" value="USER LIST" onClick="location.href='read.php'">
+        </div>
+        <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
+        <input type="hidden" name="tel" value="<?php echo $_GET['tel'] ?>">
+    </form>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../js/validate.js"></script>
+</body>
+</html>
---
+++ php/delete_done.php
@@ -0,0 +1,44 @@
+<?php
+$id = $_GET['id'];
+//DB接続情報
+$DATABASE_SOURCENAME = 'mysql:dbname=crud_foundation;hostname=localhost;';
+$DATABASE_USERNAME = 'root';
+$DATABASE_PASSWORD = 'root';
+
+try{
+    $dbh = new PDO($DATABASE_SOURCENAME,$DATABASE_USERNAME,$DATABASE_PASSWORD);
+    $sql = "select * from user";
+    $stmt = $dbh->query($sql);
+    $dbh = null;
+}catch(PDOException $e){
+    $e->getMessage();
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>DELETE</title>
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>DELETE USER</h1>
+    </header>
+    <form action="" method="post">
+
+        <div class="form-item">
+            <p>Complete dlete user</p>
+        </div>
+
+        <div class="form-item">
+            <input type="button" value="USER LIST" onClick="location.href='read.php'">
+        </div>
+    </form>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="../js/validate.js"></script>
+</body>
+</html>
